d2deb24989366c8142463385afe9964da4453088
Samuel Gomes Guimaraes <samuca.guimaraes@gmail.com>
2020-07-13 23:04:45 -0300

    - Primeira rota

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (request, response) =>{
+  response.json("Hello World");
+});
+
 app.listen(3333, () => {
   console.log('  Server Started on port 3333')
 })
